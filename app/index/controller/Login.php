b660823772146b99e745128facecbc74c3ecdda9
liyukun <1099438829@qq.com>
2022-01-16 23:32:25 +0800

    提交个人中心

--- app/index/controller/Login.php
+++ app/index/controller/Login.php
@@ -106,6 +106,24 @@ class Login extends Base
     }
 
     /**
+     * 用户中心
+     * @return string
+     * @throws Exception
+     * @author 李玉坤
+     * @date 2022-01-16 21:04
+     */
+    public function profile()
+    {
+        //清除可能存在的栏目分类树id
+        cache(Data::CURR_CATEGORY_PATENT_ID, false);
+        //模板兼容性标签
+        $this->assign('id', false);
+        $this->assign('cid', false);
+        return $this->fetch();
+    }
+
+
+    /**
      * 验证码
      * @return Response
      */
