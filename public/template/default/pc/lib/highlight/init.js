cc15e83a70a566a4b811473eec83998aa8aa54a5
yumo <yumo@im30.lan>
2023-12-31 12:59:27 +0800

    更新编辑器为wangEditer 主题支持代码高亮和复制

--- public/template/default/pc/lib/highlight/init.js
+++ public/template/default/pc/lib/highlight/init.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll('.corepress-code-pre code').forEach((block) => {
-        hljs.highlightBlock(block);
+    document.querySelectorAll('pre').forEach((pre) => {
+        pre.classList.add('ape-code-pre');
+        pre.querySelectorAll('code').forEach((block) => {
+            hljs.highlightBlock(block);
+        });
     });
-
 });
-$('.corepress-code-pre').append('<div class="code-bar"><i class="fal fa-clone code-bar-btn-copy-fonticon" title="复制"></i></div>')
-$(".corepress-code-pre code").each(function () {
+$('pre').append('<div class="code-bar"><i class="fal fa-clone code-bar-btn-copy-fonticon" title="复制"></i></div>')
+$("pre code").each(function () {
     $(this).html("<ul class='hijs-line-number'><li>" + $(this).html().replace(/\n/g, "\n</li><li>") + "\n</li></ul>");
 });
